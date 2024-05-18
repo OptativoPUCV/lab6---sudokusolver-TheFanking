46faafa973e5e46996207f7ccd22ec73a7e7d3d9
Your Name <you@example.com>
2024-05-18 23:00:22 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -46,7 +46,7 @@ void print_node(Node* n){
 
 int EstaRepetidoF(Node* aux ,int fila)
 {
-    for (int i = 0 ; i < 9 ; i++)
+    for (int i = 0 ; i < 8 ; i++)
        {
           for (int j = i + 1 ; j < 9 ; j++)
              {
@@ -104,7 +104,7 @@ int is_valid(Node* n)
    int flag = 0;
    for (int i = 0 ; i < 9 ; i++)
    {
-      if (!EstaRepetidoF(n,i) || !EstaRepetidoC(n,i))
+      if (!EstaRepetidoF(n,i))
       {
          flag = 1;
       }
